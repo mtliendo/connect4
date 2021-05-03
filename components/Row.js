6a64bee16fbe60f827c390b969a30d2d68aa70e5
Michael Liendo <mtliendo@gmail.com>
2021-05-02 23:04:40 -0500

    add cells to row file

--- components/Row.js
+++ components/Row.js
@@ -1,4 +1,5 @@
-import { Cell } from './Cell'
+import { Flex } from '@chakra-ui/layout'
+import * as gameStyles from '../styles/Home.module.css'
 
 export const Row = ({ row, play }) => {
   return (
@@ -7,5 +8,29 @@ export const Row = ({ row, play }) => {
         <Cell key={i} value={cell} columnIndex={i} play={play} />
       ))}
     </tr>
+  )
+}
+
+export const Cell = ({ value, columnIndex, play }) => {
+  let color = 'whiteCircle'
+  if (value === 1) {
+    color = 'redCircle'
+  } else if (value === 2) {
+    color = 'yellowCircle'
+  }
+
+  return (
+    <td>
+      <Flex
+        justify="center"
+        align="center"
+        className={gameStyles.gameCell}
+        onClick={() => {
+          play(columnIndex)
+        }}
+      >
+        <div className={gameStyles[color]}></div>
+      </Flex>
+    </td>
   )
 }
